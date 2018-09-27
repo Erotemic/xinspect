1f50ba017d8806a595992e88379443b17a24891a
joncrall <jon.crall@kitware.com>
2018-09-27 16:15:09 -0400

    wip

--- xinspect/__init__.py
+++ xinspect/__init__.py
@@ -1,1 +1,13 @@
-__version__ = '0.0.1.dev'
+__version__ = '0.0.2.dev'
+
+from xinspect import dynamic_kwargs
+from xinspect.dynamic_kwargs import get_func_kwargs
+
+
+get_kwargs = get_func_kwargs
+
+
+__all__ = [
+    'dynamic_kwargs',
+    'get_func_kwargs',
+]
