b80c2d4fbfb03afdd1484f6ca487eb51f3befcbb
joncrall <erotemic@gmail.com>
2019-01-13 17:42:37 -0500

    Auto argparse now uses xinspect so kwargs will work

--- xinspect/auto_argparse.py
+++ xinspect/auto_argparse.py
@@ -4,27 +4,42 @@ def auto_argparse(func):
     `argparse.ArgumentParser`.  Custom utility. Not sure where it goes yet.
 
     Probably put this in xdev or xinspect?
+
+    Args:
+        func ()
     """
     from xdoctest.docstr import docscrape_google as scrape
     import argparse
+    import ubelt as ub
+
     import inspect
+    spec = inspect.getargspec(func)
 
     # Parse default values from the function dynamically
-    spec = inspect.getargspec(func)
-    kwdefaults = dict(zip(spec.args[-len(spec.defaults):], spec.defaults))
+    try:
+        import xinspect
+        kwdefaults = xinspect.get_func_kwargs(func)
+    except Exception as ex:
+        raise
+        kwdefaults = dict(zip(spec.args[-len(spec.defaults):], spec.defaults))
 
     # Parse help and description information from a google-style docstring
     docstr = func.__doc__
     description = scrape.split_google_docblocks(docstr)[0][1][0].strip()
+
+    # TODO: allow scraping from the kwargs block as well
     google_args = {argdict['name']: argdict
                    for argdict in scrape.parse_google_args(docstr)}
+
+    argnames = ub.oset(spec.args) | ub.oset(kwdefaults)
+    argnames = (ub.oset(google_args) & argnames) | argnames
 
     # Create the argument parser and register each argument
     parser = argparse.ArgumentParser(
         description=description,
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
-    for arg in spec.args:
+    for arg in argnames:
         argkw = {}
         if arg in kwdefaults:
             argkw['default'] = kwdefaults[arg]
