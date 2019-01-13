3db3dd9a1bba698a836389a187d3720d04cf18c4
joncrall <erotemic@gmail.com>
2019-01-13 18:59:00 -0500

    wip

--- xinspect/auto_argparse.py
+++ xinspect/auto_argparse.py
@@ -1,17 +1,18 @@
 def auto_argparse(func):
     """
     Transform a function with a Google Style Docstring into an
-    `argparse.ArgumentParser`.  Custom utility. Not sure where it goes yet.
+    `argparse.ArgumentParser`.
 
-    Probably put this in xdev or xinspect?
+    TODO:
+        - [ ] Handle booleans consistently, allow --flag=True and --flag=False
 
     Args:
-        func ()
+        func (callable): function with kwargs
     """
     from xdoctest.docstr import docscrape_google as scrape
+    import ast
     import argparse
     import ubelt as ub
-
     import inspect
     spec = inspect.getargspec(func)
 
@@ -34,6 +35,10 @@ def auto_argparse(func):
     argnames = ub.oset(spec.args) | ub.oset(kwdefaults)
     argnames = (ub.oset(google_args) & argnames) | argnames
 
+    # DEBUG = 1
+    # if DEBUG:
+    #     print(ub.repr2(google_args))
+
     # Create the argument parser and register each argument
     parser = argparse.ArgumentParser(
         description=description,
@@ -46,9 +51,23 @@ def auto_argparse(func):
         if arg in google_args:
             garg = google_args[arg]
             argkw['help'] = garg['desc']
+            # print('-----')
+            # print('argkw = {}, {}'.format(arg, ub.repr2(argkw)))
             try:
-                argkw['type'] = eval(garg['type'], {})
-            except Exception:
+                if garg['type'] == 'PathLike':
+                    argkw['type'] = str
+                elif garg['type'] == 'bool':
+                    def _parse_bool(s):
+                        return bool(ast.literal_eval(s))
+                    argkw['type'] = _parse_bool
+                else:
+                    argkw['type'] = eval(garg['type'], {})
+                    # literal_eval doesnt handle types
+                    # argkw['type'] = ast.literal_eval(garg['type'])
+            except Exception as ex:
+                # print('{}, ex = {!r}'.format(arg, ex))
                 pass
+        # print('-----')
+        # print('argkw = {}, {!r}'.format(arg, argkw))
         parser.add_argument('--' + arg, **argkw)
     return parser
