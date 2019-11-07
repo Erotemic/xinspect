32fd9d4d3a8e6c6ae2bac42f6e0530103a16334d
joncrall <erotemic@gmail.com>
2019-11-06 19:11:53 -0500

    prepare 0.1.0 release

--- setup.py
+++ setup.py
@@ -9,56 +9,28 @@ Developing:
     pip install -e xinspect
 
 Pypi:
-     # Presetup
-     pip install twine
-
-     # First tag the source-code
-     VERSION=$(python -c "import setup; print(setup.version)")
-     echo $VERSION
-     git tag $VERSION -m "tarball tag $VERSION"
-     git push --tags origin master
-
-     # NEW API TO UPLOAD TO PYPI
-     # https://packaging.python.org/tutorials/distributing-packages/
-
-     # Build wheel or source distribution
-     python setup.py bdist_wheel --universal
-
-     # Use twine to upload. This will prompt for username and password
-     # If you get an error:
-     #   403 Client Error: Invalid or non-existent authentication information.
-     # simply try typing your password slower.
-     twine upload --username erotemic --skip-existing dist/*
-
-     # Check the url to make sure everything worked
-     https://pypi.org/project/xinspect/
-
-     # ---------- OLD ----------------
-     # Check the url to make sure everything worked
-     https://pypi.python.org/pypi?:action=display&name=xinspect
+    USE_GPG=True TAG_AND_UPLOAD=yes ./publish.sh
 
 """
 from setuptools import setup
 import sys
+from os.path import exists
 
 
-def parse_version(package):
+def parse_version(fpath):
     """
-    Statically parse the version number from __init__.py
-
-    CommandLine:
-        python -c "import setup; print(setup.parse_version('xinspect'))"
+    Statically parse the version number from a python file
     """
-    from os.path import dirname, join
     import ast
-    init_fpath = join(dirname(__file__), package, '__init__.py')
-    with open(init_fpath) as file_:
+    if not exists(fpath):
+        raise ValueError('fpath={!r} does not exist'.format(fpath))
+    with open(fpath, 'r') as file_:
         sourcecode = file_.read()
     pt = ast.parse(sourcecode)
     class VersionVisitor(ast.NodeVisitor):
         def visit_Assign(self, node):
             for target in node.targets:
-                if target.id == '__version__':
+                if getattr(target, 'id', None) == '__version__':
                     self.version = node.value.s
     visitor = VersionVisitor()
     visitor.visit(pt)
@@ -88,61 +60,87 @@ def parse_requirements(fname='requirements.txt'):
     Parse the package dependencies listed in a requirements file but strips
     specific versioning information.
 
+    TODO:
+        perhaps use https://github.com/davidfischer/requirements-parser instead
+
     CommandLine:
         python -c "import setup; print(setup.parse_requirements())"
     """
-    from os.path import dirname, join, exists
+    from os.path import exists
     import re
-    require_fpath = join(dirname(__file__), fname)
-    # This breaks on pip install, so check that it exists.
-    if exists(require_fpath):
-        with open(require_fpath, 'r') as f:
-            packages = []
+    require_fpath = fname
+
+    def parse_line(line):
+        """
+        Parse information from a line in a requirements text file
+        """
+        if line.startswith('-r '):
+            # Allow specifying requirements in other files
+            target = line.split(' ')[1]
+            for info in parse_require_file(target):
+                yield info
+        elif line.startswith('-e '):
+            info = {}
+            info['package'] = line.split('#egg=')[1]
+            yield info
+        else:
+            # Remove versioning from the package
+            pat = '(' + '|'.join(['>=', '==', '>']) + ')'
+            parts = re.split(pat, line, maxsplit=1)
+            parts = [p.strip() for p in parts]
+
+            info = {}
+            info['package'] = parts[0]
+            if len(parts) > 1:
+                op, rest = parts[1:]
+                if ';' in rest:
+                    # Handle platform specific dependencies
+                    # http://setuptools.readthedocs.io/en/latest/setuptools.html#declaring-platform-specific-dependencies
+                    version, platform_deps = map(str.strip, rest.split(';'))
+                    info['platform_deps'] = platform_deps
+                else:
+                    version = rest  # NOQA
+                info['version'] = (op, version)
+            yield info
+
+    def parse_require_file(fpath):
+        with open(fpath, 'r') as f:
             for line in f.readlines():
                 line = line.strip()
                 if line and not line.startswith('#'):
-                    if line.startswith('-e '):
-                        package = line.split('#egg=')[1]
-                        packages.append(package)
-                    else:
-                        # Remove versioning from the package
-                        pat = '(' + '|'.join(['>=', '==', '>']) + ')'
-                        parts = re.split(pat, line, maxsplit=1)
-                        parts = [p.strip() for p in parts]
+                    for info in parse_line(line):
+                        yield info
 
-                        package = parts[0]
-                        if len(parts) > 1:
-                            op, rest = parts[1:]
-                            if ';' in rest:
-                                # Declaring platform specific dependencies
-                                # http://setuptools.readthedocs.io/en/latest/setuptools.html#declaring-platform-specific-dependencies
-                                version, platform_deps = map(str.strip, rest.split(';'))
-                                if not sys.version.startswith('3.4'):
-                                    # apparently this breaks in 3.4
-                                    package = package + ';' + platform_deps
-                                # if platform_deps == 'platform_system=="Windows"':
-                                #     pass
-                            else:
-                                version = rest  # NOQA
-
-                        packages.append(package)
-            return packages
-    return []
+    # This breaks on pip install, so check that it exists.
+    packages = []
+    if exists(require_fpath):
+        for info in parse_require_file(require_fpath):
+            package = info['package']
+            if not sys.version.startswith('3.4'):
+                # apparently package_deps are broken in 3.4
+                platform_deps = info.get('platform_deps')
+                if platform_deps is not None:
+                    package += ';' + platform_deps
+            packages.append(package)
+    return packages
 
 
-version = parse_version('xinspect')  # needs to be a global var for git tags
+version = parse_version('xinspect/__init__.py')  # needs to be a global var for git tags
 
 if __name__ == '__main__':
     setup(
         name='xinspect',
         version=version,
         author='Jon Crall',
-        long_description=parse_description(),
-        install_requires=parse_requirements('requirements.txt'),
-        extras_require={
-            'all': parse_requirements('optional-requirements.txt')
-        },
         author_email='erotemic@gmail.com',
+        long_description=parse_description(),
+        long_description_content_type='text/x-rst',
+        install_requires=parse_requirements('requirements/runtime.txt'),
+        extras_require={
+            'all': parse_requirements('requirements.txt'),
+            'tests': parse_requirements('requirements/tests.txt'),
+            'optional': parse_requirements('requirements/optional.txt'),
+        },
         url='https://github.com/Erotemic/xinspect',
         license='Apache 2',
         packages=['xinspect'],
--- xinspect/__init__.py
+++ xinspect/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.4'
+__version__ = '0.1.0'
 
 from xinspect import dynamic_kwargs
 from xinspect.dynamic_kwargs import get_func_kwargs
